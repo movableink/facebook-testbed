d0cddf982e9edd7ec3bc9f3438ca04f47a001a8e
Michael Nutt <michael@nuttnet.net>
2012-08-18 17:50:33 -0400

    no mongoid; set fb token after every login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,10 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.where(:provider => auth['provider'], 
+    user = User.where(:provider => auth['provider'],
                       :uid => auth['uid']).first || User.create_with_omniauth(auth)
+    user.update_column(:token, auth['credentials']['token'])
+
     session[:user_id] = user.id
     if user.email.blank?
       redirect_to edit_user_path(user), :alert => "Please enter your email address."
