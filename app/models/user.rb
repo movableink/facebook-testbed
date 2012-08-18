d894835970b195e9bf9f0f83f635d65baa05af18
Michael Nutt <michael@nuttnet.net>
2012-08-18 17:28:16 -0400

    save user token when creating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
+
+      if auth['credentials']
+        user.token = auth['credentials']['token']
+      end
+
       if auth['info']
-         user.name = auth['info']['name'] || ""
-         user.email = auth['info']['email'] || ""
+        user.name = auth['info']['name'] || ""
+        user.email = auth['info']['email'] || ""
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120818203310) do
+ActiveRecord::Schema.define(:version => 20120818212529) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120818203310) do
     t.string   "uid"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "token"
   end
 
 end
