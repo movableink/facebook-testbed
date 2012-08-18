2640ccaa51a02fc981d0f300e27b48b065285c4e
Michael Nutt <michael@nuttnet.net>
2012-08-18 16:32:13 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookTestbed::Application.config.secret_token = 'fd37a87d2bf1cac15be3008441981ab8a9dd1b0b2c7d1221d2a92fb6cf4236b6047e7bb790b252ffc46d99cbda0e27d6a2d480d6ab8d66d4a83ef880d32e5fa9'
